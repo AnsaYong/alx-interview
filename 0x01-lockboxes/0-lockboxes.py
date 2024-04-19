306844def96d6d99a00bf071f0d122ebebfd277e
Ansahmbom <ansahmbomyong@gmail.com>
2024-04-19 12:29:46 +0200

    added a README.md and the pythonic implementation of the lockboxes function which checks if the provided boxes can all be opened and returns a boolean response

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+This module contains the canUnlockAll function
+which determines if all the boxes can be opened.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+
+    Args:
+        boxes (list): A list of lists. Each list contains
+        integers representing a box and the number of the
+        boxes that can be opened with the key inside the box.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+    if not boxes:
+        return False
+
+    keys = [0]
+    for key in keys:
+        for box in boxes[key]:
+            if box not in keys and box < len(boxes):
+                keys.append(box)
+
+    return len(keys) == len(boxes)
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,8 @@
+# 0x01. Lockboxes
+
+This directory contains a pythonic implementation of the Lockboxes algorithm which checks
+if a given number of boxes van all be unlocked.
+
+The following file is included:
+
+- 0-lockboxes.py
