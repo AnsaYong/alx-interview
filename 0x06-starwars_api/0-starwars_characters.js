c16e8322d086c3ce19a38bef9eae1e5ce67198ad
Ansahmbom <ansahmbomyong@gmail.com>
2024-05-20 13:55:33 +0300

    added 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,2 +1,13 @@
 #!/usr/bin/node
 console.log('Hi Ansa!')
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/film/3/';
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('Error making request:', error);
+    return;
+  }
+
+  console.log('Status Code:', response && response.statusCode);
+  console.log('Body:', body);
+});
