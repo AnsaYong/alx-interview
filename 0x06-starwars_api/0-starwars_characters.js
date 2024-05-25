43304ec0a64d5a174bbaf2dc36d4938e82cac58e
Ansa <ansahmbomyong@gmail.com>
2024-05-25 03:17:54 -0700

    updated 0-starwars_characters.js to meet semistandard requirements

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -11,20 +11,19 @@ const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
 // Function to make an HTTP GET request and return a promise
-function fetch(url) {
+function fetch (url) {
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
       if (error) reject(error);
       else if (response.statusCode !== 200) {
         reject(new Error(`Failed to fetch URL ${url}`));
-      }
-      else resolve(body);
+      } else resolve(body);
     });
   });
 }
 
 // Main function to get movie data and then character names in order
-async function main() {
+async function main () {
   try {
     const movieData = await fetch(url);
     const characters = JSON.parse(movieData).characters;
