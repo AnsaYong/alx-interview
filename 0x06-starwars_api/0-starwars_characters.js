7a5506eb75a8c41dd97292ee3f814506fdf79d6e
Ansahmbom <ansahmbomyong@gmail.com>
2024-05-25 12:57:40 +0300

    updated 0-starwars_api to use request instead of request-re

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,37 +1,47 @@
 #!/usr/bin/node
-// Import the 'request-promise' module to make HTTP requests
-const rp = require('request-promise');
+// Import the 'request' module to make HTTP requests
+const request = require('request');
 
-// Check if the movie ID is provided as a command-line argument
 if (process.argv.length < 3) {
   console.error('Please provide a movie ID as an argument');
   process.exit(1);
 }
 
-// Get the movie ID from the command-line arguments
 const movieId = process.argv[2];
-// Construct the URL to the Star Wars API
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-(async () => {
-  try {
-    // Make a GET request to the Star Wars API
-    const response = await rp(url, { json: true });
-
-    // Get list of character urls from the movie
-    const characterUrls = response.characters;
-
-    if (characterUrls.length === 0 || !characterUrls) {
-      console.log('No characters found');
-      return;
-    }
-
-    // Iterate over each character URL and make a GET request to fetch the character
-    for (const characterUrl of characterUrls) {
-      const character = await rp(characterUrl, { json: true });
-      console.log(character.name);
-    }
-  } catch (error) {
-    console.error('Error:', error);
+// Make a GET request to the Star Wars API
+request(url, { json: true }, (error, response, body) => {
+  if (error) {
+    console.error('Error retreiving movie data:', error);
+    return;
   }
-})();
+  if (response.statusCode !== 200) {
+    console.error(`Failed to fetch movie with ID ${movieId}`);
+    return;
+  }
+
+  const characterUrls = body.characters;
+
+  if (characterUrls.length === 0 || !characterUrls) {
+    console.log('No characters found');
+    return;
+  }
+
+  // Iterate over each character URL and make a GET request to fetch the character
+  characterUrls.forEach((characterUrl) => {
+    request(characterUrl, { json: true }, (error, response, body) => {
+      if (error) {
+        console.error('Error fetching character data:', error);
+        return;
+      }
+      if (response.statusCode !== 200) {
+        console.error(`Failed to fetch character with URL ${characterUrl}`);
+        return;
+      }
+
+      // Print the character name
+      console.log(body.name);
+    });
+  });
+});
